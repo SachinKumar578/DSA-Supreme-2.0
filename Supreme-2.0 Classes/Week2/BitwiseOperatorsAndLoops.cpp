114afd1a9491c7b54a77e9da0f4482e6d99a8687
SachinKumar578 <122079001+SachinKumar578@users.noreply.github.com>
2024-10-10 11:03:09 +0530

    Create BitwiseOperatorsAndLoops.cpp

---
+++ Supreme-2.0 Classes/Week2/BitwiseOperatorsAndLoops.cpp
@@ -0,0 +1,52 @@
+// Lecture name :- Bitwise operators and loops 
+// Lecture no.:- 2 , Week 2
+
+#include<iostream>
+using namespace std ;
+
+int main (){
+
+    // int a = 5 ;
+    // int b = 10 ;
+
+    // // Bitwise OR operaror
+    //  cout << (a|b) << endl ;
+
+    // // Bitwise AND operator
+    // // cout << (a&b) << endl ;
+
+    // int num = 1 ;  // why -2 comes also if we initialize this number in bool even
+
+    // cout << ~num << endl ;
+    // cout << (~num) << endl ;
+    // cout << ~(num) << endl ;
+
+
+    // int a = 5 ; 
+    // int b = -5 ;
+
+    // cout << (a^b) << endl ;
+
+    // int a = 7 ;
+    // cout << (a<<1) ;
+
+
+    // int n = 5 ;
+    // int ans = (n>>1) ;
+    // cout << ans << endl ;
+
+    // int a  = 5;
+    // cout << (a++) << endl ;
+    // cout << a ;
+
+
+   /// to check it out
+
+    int a  = 10 ; 
+   // cout << ((++a)*(a++)) << endl ;
+      cout << ((a++)*(++a));
+
+
+
+    return 0 ;
+}
