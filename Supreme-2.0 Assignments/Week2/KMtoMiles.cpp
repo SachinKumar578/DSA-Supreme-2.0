36ded4a22e215963bc9854fa0cd3c1d250a1c202
SachinKumar578 <122079001+SachinKumar578@users.noreply.github.com>
2025-01-11 08:31:31 +0530

    Add files via upload

---
+++ Supreme-2.0 Assignments/Week2/KMtoMiles.cpp
@@ -0,0 +1,19 @@
+// write  a program to convert km to miles.
+
+#include<iostream>
+using namespace std ;
+
+float KMtoMiles(float km){
+    float _1kmMile = 0.621371 ;
+    float miles = km * _1kmMile ;
+    return miles ;
+}
+
+int main(){
+    float km ;
+    cout << "Tell me km " << endl ;
+    cin >> km ;
+    float miles = KMtoMiles(km);
+    cout << "Miles = " << miles << endl ;
+
+}
